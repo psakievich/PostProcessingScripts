6e346be6726036bea2ba5beffad9d2495d264ed5
Philip Sakievich <psakiev@sanida.gov>
2020-04-27 07:48:17 -0600

    Update sample plane

--- GeneralPVPython/SamplePlane.py
+++ GeneralPVPython/SamplePlane.py
@@ -4,12 +4,12 @@ BEGIN USER INPUT
 ========================================
 """
 readFile=True
-fileName = "/Users/psakiev/Desktop/test.e"
-outputBaseName = "/Users/psakiev/Desktop/outDumm"
-sampleBounds = [0.0, 0.0, -5.0, 5.0, 0.0, 10.0]
-sampleDimensions = [1,10,10]
-resampleFullTimeSeries = False
-timeIndexToOutput = []
+fileName = "/gscratch/ahsieh/Long_Turbine2/out/Mid_2turbineJ" """.e.768.000"""
+outputBaseName = "/nscratch/psakiev/plane2"
+sampleBounds = [1430.7080, 1430.7080, 1445, 1555, 0.0, 100]
+sampleDimensions = [1,177,361]
+resampleFullTimeSeries = True
+timeIndexToOutput = [0]
 
 """
 ========================================
@@ -21,35 +21,52 @@ from paraview.simple import *
 
 if readFile:
     dataSet = OpenDataFile(fileName)
+    mergedBlocks = MergeBlocks(dataSet)
 else:
     dataSet = GetActiveSource()
+    if isinstance(dataSet, ExodusIIReader):
+        mergedBlocks = MergeBlocks(dataSet)
+    else:
+        mergedBlocks = dataSet
 
-plane = ResampleToImage(Input=dataSet)
+plane = ResampleToImage(Input=mergedBlocks)
 plane.UseInputBounds=False
 plane.SamplingBounds = sampleBounds
 plane.SamplingDimensions = sampleDimensions
 
+timeKeeper = GetTimeKeeper()
+animationScene = GetAnimationScene()
+
 HideAll()
 Show(plane)
+Render()
 
 view = GetActiveView()
 fileNameTemplate = outputBaseName + "_{timeStep}.pvti"
-writer = XMLPImageDataWriter(Input=plane)
 
 if resampleFullTimeSeries:
+    timeList = [ t for t in dataSet.TimestepValues]
     for time in dataSet.TimestepValues:
-        view.ViewTime=time
+        timeKeeper.Time=time
+        animationScene.AnimationTime=time
+        Render()
+        writer = XMLPImageDataWriter(Input=plane)
         writer.FileName = fileNameTemplate.format(
-                timeStep=dataSet.TimestepValues.index(time))
+                timeStep=timeList.index(time))
         writer.UpdatePipeline()
 
 elif timeIndexToOutput != []:
     for index in timeIndexToOutput:
-        view.ViewTime = dataSet.TimestepValues[index]
+        time = dataSet.TimestepValues[index]
+        timeKeeper.Time=time
+        animationScene.AnimationTime=time
+        Render()
+        writer = XMLPImageDataWriter(Input=plane)
         writer.FileName = fileNameTemplate.format(
                 timeStep=index)
         writer.UpdatePipeline()
 else:
+    writer = XMLPImageDataWriter(Input=plane)
     writer.FileName = fileNameTemplate.format(timeStep=0)
     writer.UpdatePipeline()
 
